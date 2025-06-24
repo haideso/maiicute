27b1d5e28f9d56c39758eb9ba5e481e64d1c0a00
haideso <dichvushark@gmail.com>
2025-06-24 11:33:36 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Beiuu à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì em thấy anh dthw, em nhắm trước rồi  <33333333',
+    mess: 'Anh biết mà hihi🥰. Thay lời muốn nóiii đến bé iu của anhhh, bấy lâu nay anh cũm có ý nma chưa biết bày tỏ như thế nào. Hôm nay liều một phen một ăn cả hai ngã về 0, mà anh nghĩ là chắc về 0 ruiii :(((',
+    messDesc: 'Beiuuu làm người yêu anh nhéee ❤️❤️❤️.',
+    btnAccept: 'Dạaaaa <3',
+    messLink: 'http://fb.com/dichvushark' //link mess của các bạn. VD: https://m.me/dichvushark
+}
